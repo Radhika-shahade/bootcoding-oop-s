09017318996a2f14a27fd3394b9f4509dab3a350
Radhika shahade <radhikashahade0804@gmail.com>
2023-06-29 22:23:54 +0530

    arraylist

---
+++ src/main/java/com/bootcoding/oops/collection/hashmap/number/NumberApp.java
@@ -0,0 +1,18 @@
+//package com.bootcoding.oops.collection.hashmap.number;
+//
+//import java.util.List;
+//import java.util.Random;
+//
+//public class NumberApp {
+//
+//    public static void main(String[] args) {
+//        NumberApp result= new NumberApp();
+//        NumberService numberService= new NumberService();
+//        List<Number> num=numberService.getRecords(20);
+//
+//    for(int i=0;i<num.size();i++)
+//    {
+//        System.out.println("result: "+ num.get(i).getNumber());
+//    }
+//    }
+//}
