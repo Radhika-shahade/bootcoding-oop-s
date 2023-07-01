373ff4a0f9383acf1aa524b4a5e9fa89c3a17483
Radhika shahade <radhikashahade0804@gmail.com>
2023-07-01 23:52:39 +0530

    arraylist

---
+++ src/main/java/com/bootcoding/oops/abstraction/Child1.java
@@ -0,0 +1,11 @@
+package com.bootcoding.oops.abstraction;
+
+public class Child1 extends Parent {
+
+    @Override
+    public void purchase(double amount) {
+        // how to purchase // child 1
+        message= "I am child1: I bought KitKat!";
+
+    }
+}
