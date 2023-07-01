48e33d70564878278d47b746e5155f4593671a68
Radhika shahade <radhikashahade0804@gmail.com>
2023-07-01 23:53:06 +0530

    abstract

---
+++ src/main/java/com/bootcoding/oops/abstraction/Child2.java
@@ -0,0 +1,10 @@
+package com.bootcoding.oops.abstraction;
+
+public class Child2 extends Parent {
+    @Override
+    public void purchase(double amount) {
+        // how to purchase // child 2
+        message= "I am child2: I bought DairyMilks!";
+
+    }
+}
