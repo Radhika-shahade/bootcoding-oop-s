9659851ce3fb1c47714567d512107f1a2cd27404
Radhika shahade <radhikashahade0804@gmail.com>
2023-07-01 23:54:58 +0530

    implementation of abstract method

---
+++ src/main/java/com/bootcoding/oops/abstraction/chocolate/Child3.java
@@ -0,0 +1,10 @@
+package com.bootcoding.oops.abstraction.chocolate;
+
+public class Child3 extends Parent {
+
+    @Override
+    public void purchase(double amount) {
+            // how to purchase // child 3
+            message= "I am child3: I bought Lays!";
+    }
+}
