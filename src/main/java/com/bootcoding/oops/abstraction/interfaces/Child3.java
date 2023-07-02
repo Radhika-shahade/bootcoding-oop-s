f0e32355ebf409b365f17f4fa1a5dc30cbe4bec2
Radhika shahade <radhikashahade0804@gmail.com>
2023-07-02 22:27:44 +0530

    implementation interface keyword

---
+++ src/main/java/com/bootcoding/oops/abstraction/interfaces/Child3.java
@@ -0,0 +1,8 @@
+package com.bootcoding.oops.abstraction.interfaces;
+
+public class Child3 implements IParent{
+    @Override
+    public void purchase(double amount) {
+        System.out.println("I am Child3 : i will buy chips!");
+    }
+}
