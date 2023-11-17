afab0a257c8401bf3f5408df7e74d7093dfcc29c
Radhika shahade <radhikashahade0804@gmail.com>
2023-11-17 18:31:49 +0530

    Inheritance

---
+++ src/main/java/com/bootcoding/oops/inheritance/FlexBanner.java
@@ -0,0 +1,16 @@
+package com.bootcoding.oops.inheritance;
+
+public class FlexBanner extends Banner {
+    double price;
+    FlexBanner(){
+        System.out.println("FlexBanner object is created");
+    }
+    public void print2()
+    {
+        System.out.println("Before - ");
+        super.print();
+        super.price = this.price;
+        System.out.println("After - ");
+        super.print();
+    }
+}
